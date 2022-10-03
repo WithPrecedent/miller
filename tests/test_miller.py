6f6fe05322914363ea2e831fab2698a008057b85
WithPrecedent <coreyrayburnyung@gmail.com>
2022-10-03 10:07:53 -0500

    updated and passed tests

--- tests/test_miller.py
+++ tests/test_miller.py
@@ -1,17 +1,31 @@
 """
 test_miller: tests functions and classes in the miller packae
 Corey Rayburn Yung <coreyrayburnyung@gmail.com>
-Copyright 2020-2021, Corey Rayburn Yung
-License: Apache-2.0 (https://www.apache.org/licenses/LICENSE-2.0)
+Copyright 2020-2022, Corey Rayburn Yung
+License: Apache-2.0
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 
 ToDo:
     
+    
 """
+from __future__ import annotations
 import dataclasses
 import inspect
 import pathlib
 import types
-from typing import Any, ClassVar, Union
+from typing import Any, ClassVar
 
 import miller
 
@@ -30,6 +44,7 @@ class TestDataclass(object):
     def do_something(self) -> None:
         return
     
+    
 class TestClass(object):
     
     a_classvar: str = 'tree'
@@ -43,9 +58,10 @@ class TestClass(object):
     
     def do_something(self) -> None:
         return
+   
     
 def test_all() -> None:
-    a_folder = pathlib.Path('.') / 'tests' / 'test_folder'
+    a_folder = pathlib.Path('.') / 'tests' / 'dummy_folder'
     a_file = pathlib.Path(a_folder) / 'dummy_module.py'
     assert miller.is_folder(item = a_folder)
     assert miller.is_module(item = a_file)
